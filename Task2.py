61a645b3dcb8bacd5ca38826993d01160823ea30
Lee Jia Wen <jlee297@e.ntu.edu.sg>
2023-10-15 08:59:01 +0800

    Add documentation and explored nodes counter

--- Task2.py
+++ Task2.py
@@ -1,25 +1,58 @@
 import json
 import heapq
 
+# Function for performing modified Uniform Cost Search with an energy constraint.
 def modified_uniform_cost_search(G, Coord, Dist, Cost, start_node, end_node, energy_constraint):
-    # Priority queue to store (combined_cost, distance, total_energy_cost, node, path) quintuplets
+    """
+    Perform Uniform Cost Search (UCS) with an energy constraint.
+
+    Args:
+        G (dict): Graph representation with nodes and their neighbors.
+        Coord (dict): Coordinate information for nodes.
+        Dist (dict): Distance between node pairs.
+        Cost (dict): Energy cost associated with edges.
+        start_node (str): Starting node.
+        end_node (str): Goal node.
+        energy_constraint (int): Maximum energy budget.
+
+    Returns:
+        explored_nodes (int): Number of nodes explored during the search.
+        shortest_solution (tuple): Tuple containing path, distance, and total energy cost.
+    """
+    # Initialize the priority queue, visited set, and exploration counter.
+    # Start from the start node with initial values of 0.
     priority_queue = [(0, 0, 0, start_node, [])]
-    visited = set()  # Set to keep track of visited nodes
+    
+    # Create a set to keep track of visited nodes.
+    visited = set()
+
+    # Counter to track the number of explored nodes.
+    explored_nodes = 0
+
+    # Initialize the variable to store the shortest solution.
     shortest_solution = None
 
+    # Main search loop.
     while priority_queue:
+
+        # Pop the node with the lowest combined cost.
         combined_cost, distance, total_energy_cost, node, path = heapq.heappop(priority_queue)
 
+        # Check if the current node is the end node.
         if node == end_node:
-            # Found a path to the end node
+            # Found a path to the end node.
             if shortest_solution is None or distance < shortest_solution[1]:
                 shortest_solution = (path, distance, total_energy_cost)
 
+        # Check if the node has been visited.
         if node in visited:
             continue  # Skip nodes that have already been visited
-
+        
+        # Mark the current node as visited and increment the exploration counter.
         visited.add(node)
+        explored_nodes += 1
 
+        # Explore neighbors of the current node.
         for neighbor in G[node]:
             edge = f"{node},{neighbor}"
             neighbor_cost = Cost.get(edge, 0)
@@ -27,17 +60,34 @@ def modified_uniform_cost_search(G, Coord, Dist, Cost, start_node, end_node, ene
             new_energy_cost = total_energy_cost + neighbor_cost
             new_distance = distance + neighbor_distance
 
+            # Check if the new energy cost is within the energy constraint.
             if new_energy_cost <= energy_constraint:
-                combined_cost = new_distance + 0.05*new_energy_cost  # You can adjust this cost function to balance distance and energy
+
+                # Calculate the new combined cost, considering both distance and energy cost.
+                combined_cost = new_distance + 0.05 * new_energy_cost
+
+                # Add the neighbor to the priority queue for further exploration.
                 heapq.heappush(priority_queue, (combined_cost, new_distance, new_energy_cost, neighbor, path + [node]))
 
-    return shortest_solution  # Return the solution with the shortest distance within the energy constraint
+    # Return the number of explored nodes and the shortest solution.
+    return explored_nodes, shortest_solution
 
+# Function to format the path as a string.
 def format_path(path):
+    """
+    Format a list of nodes as a string path.
+
+    Args:
+        path (list): List of nodes representing a path.
+
+    Returns:
+        formatted_path (str): String representation of the path.
+    """
     return '->'.join(path)
 
+# Main function to execute the search.
 def main():
-    # Load data from JSON files
+    # Load data from JSON files.
     with open('Coord.json', 'r') as coord_file:
         Coord = json.load(coord_file)
 
@@ -54,19 +104,32 @@ def main():
     end_node = "50"
     energy_constraint = 287932
 
-    # Perform modified UCS with energy constraint
-    shortest_solution = modified_uniform_cost_search(G, Coord, Dist, Cost, start_node, end_node, energy_constraint)
+    # Perform modified UCS with energy constraint.
+    explored_nodes, shortest_solution = modified_uniform_cost_search(G, Coord, Dist, Cost, start_node, end_node, energy_constraint)
 
+    # Print header for the task.
+    print("*"*10 + " Task 2 - Uniformed Cost Search with Energy Constraint of 287932 " + "*"*10)
+    
     if shortest_solution is None:
         print(f"No path found from node 1 to node 50 within the energy constraint of {energy_constraint}.")
     else:
-        formatted_path = format_path(shortest_solution[0]+[end_node])
+        formatted_path = format_path(shortest_solution[0] + [end_node])
         shortest_distance = shortest_solution[1]
         total_energy_cost = shortest_solution[2]
 
+        # Print output with formatting.
+        print()
         print(f"Shortest path: S->{formatted_path}->T.")
+        print()
         print(f"Shortest distance: {shortest_distance}.")
+        print()
         print(f"Total energy cost: {total_energy_cost}.")
+        print()
+        print(f"Number of explored nodes: {explored_nodes}")
+        print()
+    
+    # Print footer.
+    print("*"*30)
 
 if __name__ == "__main__":
     main()

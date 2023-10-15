f41bd40a3fc8ed8b77a916b65d3fe4e8406c4af1
kimberlyk82 <kimby.khow@gmail.com>
2023-10-15 12:49:06 +0800

    documentation + formatted

--- Task1.py
+++ Task1.py
@@ -2,6 +2,22 @@ import json
 import heapq
 
 def uniform_cost_search(G, Coord, Dist, start_node, end_node):
+
+    """
+    Perform Uniform Cost Search (UCS) to find the shortest path from the start node to the end node.
+
+    Parameters:
+    - G (dict): Graph representation with nodes and their neighbors.
+    - Coord (dict): Coordinate information for nodes.
+    - Dist (dict): Distance between node pairs.
+    - start_node (str): Starting node.
+    - end_node (str): Goal node.
+
+    Returns:
+    - cost (int): Total cost of the shortest path from start_node to end_node.
+    - path (list): List of nodes representing the shortest path.
+    """
+    
     priority_queue = [(0, start_node, [])]  # Priority queue to store (cost, node, path) triplets
     visited = set()  # Set to keep track of visited nodes
 
@@ -24,6 +40,15 @@ def uniform_cost_search(G, Coord, Dist, start_node, end_node):
     return float("inf"), []  # No path found to the end node
 
 def format_path(path):
+    """
+    Format a list of nodes as a string path.
+
+    Args:
+        path (list): List of nodes representing a path.
+
+    Returns:
+        formatted_path (str): String representation of the path.
+    """
     return '->'.join(path)
 
 def main():
@@ -46,15 +71,23 @@ def main():
     # Perform UCS
     shortest_path_cost, shortest_path = uniform_cost_search(G, Coord, Dist, start_node, end_node)
 
+    #print header for task
+    print("*"*10 + " Task 1 - Uniformed Cost Search " + "*"*10)
+
     if shortest_path_cost == float("inf"):
         print("No path found from node 1 to node 50.")
     else:
-        formatted_path = format_path(shortest_path)
+        formatted_path = format_path(shortest_path + [end_node])
         total_energy_cost = sum(Cost.get(f"{shortest_path[i]},{shortest_path[i+1]}", 0) for i in range(len(shortest_path)-1))
        
+        print()
         print(f"Shortest path: S->{formatted_path}->T.")
         print(f"Shortest distance: {shortest_path_cost}.")
         print(f"Total energy cost: {total_energy_cost}.")
+        print()
+
+        # Print footer.
+        print("*"*30)
 
 if __name__ == "__main__":
     main()
